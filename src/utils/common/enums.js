94a83160e9d6d0506813ee30523f07997549d0d4
Priyanshu0512 <priyanshuk0595@gmail.com>
2024-01-10 12:57:49 +0530

    Added Bookings model and utils folder.

---
+++ src/utils/common/enums.js
@@ -0,0 +1,17 @@
+const SEAT_TYPE ={
+    BUSINESS: 'business',
+    PREMIUM_ECONOMY : 'premium-economy',
+    ECONOMY: 'economy',
+    FIRST_CLASS: 'first-class'
+}
+
+const BOOKING_STATUS={
+    BOOKED: 'booked',
+    PENDING: 'pending',
+    INITIATED: 'initiated',
+    CANCELLED: 'cancelled'
+}
+module.exports= {
+    SEAT_TYPE,
+    BOOKING_STATUS
+}
